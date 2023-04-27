9dec70d946ed841134e3edbd75ea9826c25c8257
rezaul choudhury <rofy2009@gmail.com>
2023-04-27 22:00:42 +0100

    home and product list componenet been comented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 
 const App = () => {
