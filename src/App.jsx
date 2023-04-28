fa9aa18d1917eb1b613e1a15cbf12433641431b5
rezaul choudhury <rofy2009@gmail.com>
2023-04-28 02:29:22 +0100

    register, login, cart pages been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-// import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 const App = () => {
   return (
       <div>
-        {/*<Home />*/}
-        {/*<ProductList />*/}
-        {/*<Product />*/}
-        {/*  <Register />*/}
-        {/*  <Login />*/}
+        <Home />
+        <ProductList />
+        <Product />
+          <Register />
+          <Login />
           <Cart />
       </div>
 
